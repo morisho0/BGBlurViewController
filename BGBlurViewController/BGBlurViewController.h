f51dfdea6261234fa93b2a6820cc0c218f95c5cf
Shota <b.morisho0@gmail.com>
2016-07-19 19:26:48 +0900

    Initial Commit

---
+++ BGBlurViewController/BGBlurViewController.h
@@ -0,0 +1,19 @@
+//
+//  BGBlurViewController.h
+//  BGBlurViewController
+//
+//  Created by Shota on 2016/07/19.
+//  Copyright © 2016年 morisho0. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BGBlurViewController.
+FOUNDATION_EXPORT double BGBlurViewControllerVersionNumber;
+
+//! Project version string for BGBlurViewController.
+FOUNDATION_EXPORT const unsigned char BGBlurViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BGBlurViewController/PublicHeader.h>
+
+
